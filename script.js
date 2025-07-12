9d2bad40666b58f3b6d5f82da23e9eabbe8a0032
Nimesh04 <nimeshb907@gmail.com>
2025-07-12 18:18:54 -0500

    Fixed issue of first few columns color going 1.0
    Before hand when your mouse entered the div first
    few columns would instantly go to full color while
    the others took sometime, it was because of multiple
    listeners attached to the row because of hoverEffect
    being called inside the for loop.

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let row, column;
 // Creates the random numbers and returns a list for the rgb 
 
 function randomColor(a = 0.1){
-    let r = parseInt(Math.random() * 1000 % 257);
-    let g = parseInt(Math.random() * 1000 % 257);
-    let b = parseInt(Math.random() * 1000 % 257);
-    return `rgba(${r},${g}, ${b}, ${a})`;
+    let r = parseInt(Math.random() * 255);
+    let g = parseInt(Math.random() * 255);
+    let b = parseInt(Math.random() * 255);
+    return `rgba(${r},${g},${b},${a})`;
 }
 
 // Resets the grid according the size given by the user, but has to be between 1 to 100
@@ -39,10 +39,14 @@ function createGrid(size = 16){
             column.appendChild(row);
     }
     div.appendChild(column);
+    }
     hoverEffect();
 }
-}
 
+
+// Begin
+
+createGrid();
 
 // Gets the value of rgb and then checks if alpha has a value that is less than 1.0, if it has then it increase it
 
@@ -50,16 +54,25 @@ function increaseOpacity(valueOfBg){
     let red, green,blue, alpha;
     [red, green, blue, alpha] = valueOfBg.match(/[0-9]+(\.[0-9])?/g);
     if(alpha === undefined) alpha = 1.0;
-    if(alpha < 1.0) alpha = parseFloat(alpha) + 0.1;
-    return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    if(alpha < 1.0)alpha = parseFloat(alpha) + 0.1;
+    return `rgba(${red}, ${green}, ${blue}, ${alpha.toString()})`;
+}
+
+// Decrease the opacity to earse the color
+
+function decreaseOpacity(bgValue){
+    let red, green, blue, alpha;
+    [red,green,blue,alpha] = bgValue.match(/[0-9]+(\.[0-9])?/g);
+    if(alpha == undefined) alpha = 0.1;
+    if(alpha > 0.1 & alpha <= 1.0) alpha = parseFloat(alpha) -0.1;
+    return `rgba(${red},${green},${blue},${alpha})`;
 }
 
 // first it checks if the background color is empty(white), if it is, then it gets the random color and assigns it
 // If it already has color, then takes that color and calls the function to increase the opacity.
 
 function paintSquare(element){
-    console.log("Hovering");
-    if(element.style.backgroundColor == ""){
+    if(window.getComputedStyle(element).backgroundColor == EMPTY_COLOR ){
         element.style.backgroundColor = randomColor(0.1);
     }else{
         element.style.backgroundColor = increaseOpacity(element.style.backgroundColor);
@@ -69,20 +82,29 @@ function paintSquare(element){
 // When the mouse hovers over the squares, it paints that square
 
 function hoverEffect(){
-    const hoverDiv = document.querySelectorAll("div.row"); 
+    const hoverDiv = document.querySelectorAll("div.sketchContainer div.row"); 
     hoverDiv.forEach(element => {
-        element.addEventListener("mouseenter",()=>paintSquare(element));
+        element.addEventListener("mouseenter",()=>setTimeout(paintSquare(element), 5000));
     });
 }
 
-// Begin
-
-createGrid();
-const EMPTY_COLOR = "rgba(0, 0, 0, 0)";
+function isPressed(e){
+    if(e.shiftKey){
+        console.log("Pressed shift:", e.shiftKey);
+        return shiftKeyPressed = true;
+    }else if(e.ctrlKey){
+        console.log("Pressed ctrl:", e.ctrlKey);
+        return ctrlKeyPressed = true;
+    }else{
+        return false;
+    }
+}
 
 
 // Captures the click on the reset button
-
+let shiftKeyPressed = false;
+let ctrlKeyPressed = false;
+const EMPTY_COLOR = "rgb(255, 255, 255)";
 resetBtn.addEventListener("click", () => resetGrid());
 
 
