e6d40ef1f82db7b4a91d2d56990c45bae02beaef
Nimesh04 <nimeshb907@gmail.com>
2025-07-12 21:12:24 -0500

    Redesigned the page & added the multi-color option
    Can paint with either a shade of black or random
    color, based on the option that is chose.

--- script.js
+++ script.js
@@ -2,9 +2,38 @@
 
 const div = document.querySelector(".sketchContainer");
 const newBoard = document.querySelector("#popUp");
+const colored = document.querySelector("#colored");
+const monotone = document.querySelector("#monotone");
 let shiftKeyPressed = false;
 let ctrlKeyPressed = false;
-let row, column;
+let row, column, coloredOption, monotoneOption;
+
+
+colored.addEventListener("click", coloredFun)
+monotone.addEventListener("click", monotoneColor)
+
+// Set the Monotone color option on 
+
+function monotoneColor(){
+    coloredOption = false;
+    monotoneOption = true;
+    colored.style.background = "rgb(255, 255, 255)";
+    colored.style.color = "black";
+    monotone.style.backgroundColor = "rgb(61, 61, 61)";
+    monotone.style.color = "white";
+}
+
+// Set the Colored option on
+
+function coloredFun(){
+    monotoneOption = false;
+    coloredOption = true;
+    console.log("colored option enabled");
+    monotone.style.backgroundColor = "rgb(255, 254, 254)";
+    monotone.style.color = "black";
+    colored.style.background = "linear-gradient(45deg,rgb(148, 68, 170) 0%,rgb(224, 76, 40) 100%)";
+    colored.style.color      = "white";
+}
 
 // Creates the random numbers and returns a list for the rgb 
 
@@ -31,7 +60,7 @@ function createNewBoard(){
 
 // From the size given by the user, this creates the grid
 
-function createGrid(size = 16){
+function createGrid(size = 20){
     for(let i = 0; i < size; i++){
         column = document.createElement("div");
         column.classList.add("column");
@@ -51,16 +80,23 @@ function createGrid(size = 16){
 createGrid();
 
 // Gets the value of rgb and then checks if alpha has a value that is less than 1.0, if it has then it increase it
+// Also checks and increase the opacity based on which option is set at the moment, doesn't increase the opacity for colored if monotone
+// is the choice and vice versa
 
 function increaseOpacity(valueOfBg){
     let red, green,blue, alpha;
     [red, green, blue, alpha] = valueOfBg.match(/[0-9]+(\.[0-9])?/g);
+    const isBlack = [red, green, blue]
+                    .map(v => parseInt(v))
+                    .every(color => color === 0);
+    if(monotoneOption && !isBlack) return;
+    if(coloredOption && isBlack) return;
     if(alpha === undefined) alpha = 1.0;
     if(alpha < 1.0)alpha = parseFloat(alpha) + 0.1;
     return `rgba(${red}, ${green}, ${blue}, ${alpha.toString()})`;
 }
 
-// Decrease the opacity to earse the color
+// Decrease the opacity to erase the color
 
 function decreaseOpacity(bgValue){
     let red, green, blue, alpha;
@@ -78,7 +114,8 @@ function paintSquare(element){
     if(shiftKeyPressed){
         ctrlKeyPressed = false;
         if(window.getComputedStyle(element).backgroundColor == EMPTY_COLOR){
-            element.style.backgroundColor = randomColor(0.1);
+            if(coloredOption) element.style.backgroundColor = randomColor(0.1);
+            if(monotoneOption) element.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
         }else{
             element.style.backgroundColor = increaseOpacity(element.style.backgroundColor);
         }
@@ -103,21 +140,25 @@ function hoverEffect(){
 }
 
 
-// Captures the click on the reset button
-const EMPTY_COLOR = "rgb(255, 255, 255)";
+// Event Listener to check when you press the key
+
 document.addEventListener("keydown", trigger =>{
     if(trigger.shiftKey) shiftKeyPressed = true;
     if(trigger.ctrlKey) ctrlKeyPressed = true;
 });
 
+// Event listener to check when you release the key
 
 document.addEventListener("keyup", trigger =>{
     if(trigger.key == "Shift") shiftKeyPressed = false;
     if(trigger.key == "Control") ctrlKeyPressed = false;
 
 })
-shiftKeyPressed,ctrlKeyPressed = false;
+
+
+const EMPTY_COLOR = "rgb(255, 255, 255)";
+const MONOTONE_COLOR = "rgb(0, 0, 0)"
+// Captures the click on the reset button
 newBoard.addEventListener("click", () => createNewBoard());
 
-
-
+monotoneColor();
